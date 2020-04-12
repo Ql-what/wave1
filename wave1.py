dd95771667da1dfc8db98eacd5057c423eedb758
Ql-what <62632626+Ql-what@users.noreply.github.com>
2020-04-11 22:16:54 -0400

    Add files via upload

---
+++ wave1.py
@@ -0,0 +1,4 @@
+length = int(input("the length of the farmer's field in feet is " ))
+width = int(input("the width of the farmer's field in feet is " ))
+area = length * width/43560
+print ("the area of the field is", area , "acre" )
